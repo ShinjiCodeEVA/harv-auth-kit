cd2d43be6162569960308be74b9e2f9b2fc25f2c
ShinjiCodeEVA <solisharvie1@gmail.com>
2024-10-19 16:01:33 +0800

    feat(auth): configAuth defined types

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,78 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import React, { useMemo } from "react";
+import {
+  QueryFunction,
+  QueryKey,
+  MutationFunction,
+  UseQueryOptions,
+  useQuery,
+  UseMutationOptions,
+  useQueryClient,
+  useMutation,
+} from "@tanstack/react-query";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+export interface ReactQueryAuthConfig<
+  User,
+  LoginCredentials,
+  RegisterCredentials,
+> {
+  userFn: QueryFunction<User, QueryKey>;
+  loginFn: MutationFunction<User, LoginCredentials>;
+  registerFn: MutationFunction<User, RegisterCredentials>;
+  userKey?: QueryKey;
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+export function configureAuth<
+  User,
+  Error,
+  LoginCredentials,
+  RegisterCredentials,
+>(config: ReactQueryAuthConfig<User, LoginCredentials, RegisterCredentials>) {
+  const {
+    userFn,
+    loginFn,
+    registerFn,
+    userKey = ["authenticated-user"],
+  } = config;
+
+  // NOTE
+  // useUser must accept UseQueryOptions omitting the  "queryKey" | "queryFn" since its manage in the hook
+  const useUser = (
+    options: Omit<
+      UseQueryOptions<User, Error, User, QueryKey>,
+      "queryKey" | "queryFn"
+    >,
+  ) =>
+    useQuery({
+      queryKey: userKey,
+      queryFn: userFn,
+      ...options,
+    });
+
+  // NOTE useLogin accepts UseMutationOptions omitting the "mutationFn"
+  // it sets the data of the user through queryClient with the querykey that is used to cached when user uses the useUser
+  const useLogin = (
+    options: Omit<
+      UseMutationOptions<User, Error, LoginCredentials>,
+      "mutationFn"
+    >,
+  ) => {
+    const queryClient = useQueryClient();
+
+    const setUser = React.useCallback(
+      (data: User) => queryClient.setQueryData(userKey, data),
+      [queryClient],
+    );
+
+
+    // options?.onSuccess -> is user-defined when invoking useLogin, 
+    // onSuccess -> while the other is built-in to the mutation logic itself (onSuccess in useMutation).
+    return useMutation({
+        mutationFn: loginFn,
+        ...options,
+        onSuccess: (user, ...rest) => {
+            setUser(user);
+            options?.onSuccess?.(user, ...rest); 
+        }
+    })
+  };
+}
